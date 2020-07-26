26842cbab383f6a2ba7b1e423cec9a7af419dec0
Nikita Balikhin <balikhin.n.ev@sberbank.ru>
2020-07-26 22:16:44 +0300

    Development stack upgrade

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,7 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 import App from './App';
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+import './index.scss';
 
-ReactDOM.render(
-  	<App />,
-  	document.getElementById('root') as HTMLElement
-);
-registerServiceWorker();
-
-if(module.hot) {
-  	module.hot.accept('./App', () => {
-    	ReactDOM.render(
-      		<App />,
-      		document.getElementById('root') as HTMLElement
-    	);
-  	})
-}
+ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
